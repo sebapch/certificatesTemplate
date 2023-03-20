27c2021c3b8cf97a3f847476c83975d7c9655e99
Sebastian Medina <39243497+sebapch@users.noreply.github.com>
2023-03-20 15:30:52 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Foto from './assets/Foto1.png'
+
+function App() {
+  return (
+    <div className="App">
+      <img src={Foto} alt='' />
+    </div>
+  );
+}
+
+export default App;
